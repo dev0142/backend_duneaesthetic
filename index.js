c8df453094e194da71c5dc4d8641ac41b3f63aad
Devashish Bhandari <devashishbhandari09@gmail.com>
2023-06-30 12:19:33 +0530

    fixed some more

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const bodyParser = require('body-parser');
 const app = express();
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb" }));
+app.use(express.json());
+app.use(express.urlencoded());
 app.use(cookieParser());
 const router = express.Router();
 
