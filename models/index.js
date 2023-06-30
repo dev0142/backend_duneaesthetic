33ea9d82df6d60ccb9c75be05fd212268538b66a
Devashish Bhandari <devashishbhandari09@gmail.com>
2023-06-30 12:37:06 +0530

    last try

--- models/index.js
+++ models/index.js
@@ -1,17 +1,18 @@
 const dbConfig = require("../config/db.config.js");
 
 const Sequelize = require("sequelize");
+const mysql2 = require("mysql2");
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.dialect,
   operatorsAliases: false,
-
+  dialectModule: mysql2,
   pool: {
     max: dbConfig.pool.max,
     min: dbConfig.pool.min,
     acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle
-  }
+    idle: dbConfig.pool.idle,
+  },
 });
 
 const db = {};
